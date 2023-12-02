7b99be4aaf7ca18321552b0d5953a474a7be2fcb
Kyle Flores <6565728+kyflores@users.noreply.github.com>
2023-12-01 20:11:41 -1000

    Add day template

---
+++ 2023/day0.py
@@ -0,0 +1,25 @@
+# TEMPLATE
+
+
+def problem_a(data):
+    return "a"
+
+
+def problem_b(data):
+    return "b"
+
+
+# TEST
+import unittest
+
+
+class TestProblem(unittest.TestCase):
+    def test_samples_a(self):
+        self.assertEqual(True, True)
+
+    def test_samples_b(self):
+        self.assertEqual(True, True)
+
+
+if __name__ == "__main__":
+    unittest.main()
