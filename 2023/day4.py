1c8d0f888e6989310d034c68093cb585ae5e0d31
Kyle Flores <6565728+kyflores@users.noreply.github.com>
2023-12-03 23:41:03 -1000

    Day 4 B

--- 2023/day4.py
+++ 2023/day4.py
@@ -26,7 +26,41 @@ def problem_a(data):
 
 
 def problem_b(data):
-    return "b"
+    # First preprocess all the lines into one data structure.
+    all_cards = []
+    lines = data.strip("\n").split("\n")
+    lines = [x.split(":")[1].split("|") for x in lines]
+    for ix, (left, right) in enumerate(lines, start = 1):
+        right = index_have(right)
+        left = [int(x) for x in re.findall(r"\d+", left)]
+        # total = sum([(1 if (right.get(x) is not None) else 0) for x in right])
+        all_cards.append({"ix": ix, "left": left, "right": right})
+
+
+    # Cards is just one line when first called, but will expand as we recurse
+    def count(card, all_cards) -> int:
+        total = [(1 if (card['right'].get(x) is not None) else 0) for x in card['left']]
+        total = sum(total)
+        # print("Card {} had {} matches".format(card, total))
+
+        # If this card had no matches, return immediately as the base case.
+        if total == 0:
+            return 0
+
+        dups_total = 0
+        for x in all_cards[card['ix'] : card['ix'] + total]:
+            dups_total += count(x, all_cards)
+
+        return total + dups_total
+
+    # Since my recursive function only counts the dups that result from a given card,
+    # we start the count at len(all_cards) to include the ones we already start with.
+    out = len(all_cards)
+    for c in all_cards:
+        res = count(c, all_cards)
+        out += res
+
+    return out
 
 
 # TEST
@@ -50,7 +84,18 @@ class TestProblem(unittest.TestCase):
         self.assertEqual(res, 13)
 
     def test_samples_b(self):
-        self.assertEqual(True, True)
+        data = dedent(
+            """\
+            Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+            Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+            Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+            Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+            Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+            Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+            """
+        )
+        res = problem_b(data)
+        self.assertEqual(res, 30)
 
 
 if __name__ == "__main__":
